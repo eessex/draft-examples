cfbd084b31e72ecd0aa5ade569c5692b365ae8ce
Eve Essex <eve.essex@gmail.com>
2018-06-28 18:45:34 -0400

    better homepage summaries

--- src/App.js
+++ src/App.js
@@ -11,19 +11,23 @@ class App extends Component {
           <h1>Draft Examples</h1>
         </HeaderContainer>
 
-        <EditorContainer>
-          <h3>Basic Editor</h3>
-          <p>Has linebreaks and spellcheck, no styles.</p>
-          <BasicEditor />
-        </EditorContainer>
+        <ContentContainer>
+          <EditorContainer>
+            <h2>Basic Editor</h2>
+            <p>Has linebreaks and spellcheck, no styles.</p>
+            <p><b>⌘</b> <code>undo, redo, copy, paste, select-all</code></p>
+            <BasicEditor />
+          </EditorContainer>
 
-        <EditorContainer>
-          <h3>Basic Editor with Content</h3>
-          <p>Extends basic editor to initialize with existing content.</p>
-          <BasicWithContent
-            content="Content editable is so passe"
-          />
-        </EditorContainer>
+          <EditorContainer>
+            <h2>Basic Editor with Content</h2>
+            <p>Extends basic editor to initialize with existing content, passed as <code>props.content</code>.</p>
+            <p><b>⌘</b> <code>undo, redo, copy, paste, select-all</code></p>
+            <BasicWithContent
+              content="Content editable is so passe."
+            />
+          </EditorContainer>
+        </ContentContainer>
       </div>
     )
   }
@@ -32,15 +36,22 @@ class App extends Component {
 export default App
 
 const HeaderContainer = styled.div`
-  background-color: #222;
   padding: 20px;
-  color: white;
-  text-align: center;
+  h1 {
+    margin: 0;
+    border-bottom: 2px solid;
+    display: initial;
+  }
+`
+const ContentContainer = styled.div`
+  padding: 20px;
+  max-width: 600px;
+  margin: 0 auto;
 `
 
 const EditorContainer = styled.div`
-  padding: 40px;
-  max-width: 600px;
-  margin: 0 auto;
-  text-align: left;
+  padding-bottom: 40px;
+  h2 {
+    margin-bottom: 0;
+  }
 `
--- src/styles.jsx
+++ src/styles.jsx
@@ -1,13 +1,17 @@
 import styled from 'styled-components'
 
+const colors = {
+  gray: '#ccc'
+}
+
 export const EditorContainer = styled.div`
-  border: 1px solid #ccc;
+  border: 1px solid ${colors.gray};
   min-height: 5em;
   padding: 10px;
   position: relative;
 
   .public-DraftEditorPlaceholder-root {
-    color: #ccc;
+    color: ${colors.gray};
     position: absolute;
   }
 `
